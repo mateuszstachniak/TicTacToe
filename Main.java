1b83fb36c77c00dc11cbbcc4de435ccdcafde85b
mateuszstachniak <91155660+mateuszstachniak@users.noreply.github.com>
2021-11-07 16:35:20 +0100

    Update files
    Working TicTacToe game

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
-package com.company;
+package tictactoe;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter cells: ");
-        String tictactoe = scanner.nextLine();
+//Generate clean board
+        String tictactoe = "         ";
         int a = tictactoe.length();
         char ch;
         char[] symbol = new char[9];
@@ -15,131 +15,128 @@ public class Main {
                 ch = tictactoe.charAt(i);
                 symbol[i] = ch;
             }
-            System.out.println("---------");
-            System.out.println("| " + symbol[0] + " " + symbol[1] + " " + symbol[2] + " |");
-            System.out.println("| " + symbol[3] + " " + symbol[4] + " " + symbol[5] + " |");
-            System.out.println("| " + symbol[6] + " " + symbol[7] + " " + symbol[8] + " |");
-            System.out.println("---------");
         } else {
             System.exit(0);
         }
-        boolean wrongInput = true;
-        boolean wrongSymbolY = true;
-        boolean wrongSymbolX = true;
-        do {
-            int y = 0;
-            int x = 0;
-            System.out.print("Enter the coordinates: ");
-
-            try {
-                y = Integer.parseInt(scanner.next());
-            } catch (NumberFormatException e) {
-                wrongSymbolY = false;
-            }
-
-            try {
-                x = Integer.parseInt(scanner.next());
-            } catch (NumberFormatException e) {
-                wrongSymbolX = false;
-            }
-
-            if (wrongSymbolY == false || wrongSymbolX == false) {
-                System.out.println("You should enter numbers!");
-                continue;
-            }
-
-            if (y < 1 || y > 3 || x < 1 || x > 3) {
-                System.out.println("Coordinates should be from 1 to 3!");
-                continue;
-            }
-
-            int x1 = 0;
-            if (x >= 1 && x <= 3) {
-                if (y == 1) {
-                    x1 = 0 + x - 1;
-                } else if (y == 2) {
-                    x1 = 3 + x - 1;
-                } else if (y == 3) {
-                    x1 = 6 + x - 1;
-                }
-            }
-
-            if (symbol[x1] == 'X' || symbol[x1] == 'O') {
-                System.out.println("This cell is occupied! Choose another one!");
-                continue;
-            } else {
-                symbol[x1] = 'X';
-                wrongInput = false;
-            }
-        } while (wrongInput);
-
+//Print clean board
         System.out.println("---------");
         System.out.println("| " + symbol[0] + " " + symbol[1] + " " + symbol[2] + " |");
         System.out.println("| " + symbol[3] + " " + symbol[4] + " " + symbol[5] + " |");
         System.out.println("| " + symbol[6] + " " + symbol[7] + " " + symbol[8] + " |");
         System.out.println("---------");
-
-/*        boolean xWins = false;
+//Start game
+        boolean wrongInput = true;
+        boolean wrongSymbolY = true;
+        boolean wrongSymbolX = true;
+        boolean xWins = false;
         boolean oWins = false;
         boolean draw = false;
-        boolean impossible = false;
+        int moveCounter = 0;
+        do {
+            do {
+                int y = 0;
+                int x = 0;
+                System.out.print("Enter the coordinates: ");
+//Catch bad coordinates
+                try {
+                    y = Integer.parseInt(scanner.next());
+                } catch (NumberFormatException e) {
+                    wrongSymbolY = false;
+                }
+                try {
+                    x = Integer.parseInt(scanner.next());
+                } catch (NumberFormatException e) {
+                    wrongSymbolX = false;
+                }
+                if (wrongSymbolY == false || wrongSymbolX == false) {
+                    System.out.println("You should enter numbers!");
+                    continue;
+                }
+                if (y < 1 || y > 3 || x < 1 || x > 3) {
+                    System.out.println("Coordinates should be from 1 to 3!");
+                    continue;
+                }
+//Coordinates conversion to 1d array
+                int x1 = 0;
+                if (x >= 1 && x <= 3) {
+                    if (y == 1) {
+                        x1 = 0 + x - 1;
+                    } else if (y == 2) {
+                        x1 = 3 + x - 1;
+                    } else if (y == 3) {
+                        x1 = 6 + x - 1;
+                    }
+                }
+//Saving player move
+                if (symbol[x1] == 'X' || symbol[x1] == 'O') {
+                    System.out.println("This cell is occupied! Choose another one!");
+                    continue;
+                } else {
+                    if (moveCounter % 2 == 0) {
+                        symbol[x1] = 'X';
+                    } else {
+                        symbol[x1] = 'O';
+                    }
+                    moveCounter++;
+                    wrongInput = false;
+                }
+            } while (wrongInput);
+            System.out.println("---------");
+            System.out.println("| " + symbol[0] + " " + symbol[1] + " " + symbol[2] + " |");
+            System.out.println("| " + symbol[3] + " " + symbol[4] + " " + symbol[5] + " |");
+            System.out.println("| " + symbol[6] + " " + symbol[7] + " " + symbol[8] + " |");
+            System.out.println("---------");
 //Check X, 0
-        for (int i = 1; i < symbol.length; ) {
-            if (symbol[i - 1] == 'X' && symbol[i] == 'X' && symbol[i + 1] == 'X') {
+            for (int j = 1; j < symbol.length; ) {
+                if (symbol[j - 1] == 'X' && symbol[j] == 'X' && symbol[j + 1] == 'X') {
+                    xWins = true;
+                } else if (symbol[j - 1] == 'O' && symbol[j] == 'O' && symbol[j + 1] == 'O') {
+                    oWins = true;
+                }
+                j++;
+                j++;
+                j++;
+            }
+            for (int j = 0; j <= 2; j++) {
+                if (symbol[j] == 'X' && symbol[j + 3] == 'X' && symbol[j + 6] == 'X') {
+                    xWins = true;
+                } else if (symbol[j] == 'O' && symbol[j + 3] == 'O' && symbol[j + 6] == 'O') {
+                    oWins = true;
+                }
+            }
+            if (symbol[0] == 'X' && symbol[4] == 'X' && symbol[8] == 'X') {
                 xWins = true;
-            } else if (symbol[i - 1] == 'O' && symbol[i] == 'O' && symbol[i + 1] == 'O') {
+            } else if (symbol[0] == 'O' && symbol[4] == 'O' && symbol[8] == 'O') {
+                oWins = true;
+            } else if (symbol[2] == 'X' && symbol[4] == 'X' && symbol[6] == 'X') {
+                xWins = true;
+            } else if (symbol[2] == 'O' && symbol[4] == 'O' && symbol[6] == 'O') {
                 oWins = true;
             }
-            i++;
-            i++;
-            i++;
-        }
-        for (int i = 0; i <= 2; i++) {
-            if (symbol[i] == 'X' && symbol[i + 3] == 'X' && symbol[i + 6] == 'X') {
-                xWins = true;
-            } else if (symbol[i] == 'O' && symbol[i + 3] == 'O' && symbol[i + 6] == 'O') {
-                oWins = true;
-            }
-        }
-        if (symbol[0] == 'X' && symbol[4] == 'X' && symbol[8] == 'X') {
-            xWins = true;
-        } else if (symbol[0] == 'O' && symbol[4] == 'O' && symbol[8] == 'O') {
-            oWins = true;
-        } else if (symbol[2] == 'X' && symbol[4] == 'X' && symbol[6] == 'X') {
-            xWins = true;
-        } else if (symbol[2] == 'O' && symbol[4] == 'O' && symbol[6] == 'O') {
-            oWins = true;
-        }
 //Counter
-        int counterX = 0;
-        int counterO = 0;
-        int counterSpace = 0;
-        for (char occ : symbol) {
-            if (occ == 'X') {
-                counterX++;
-            } else if (occ == 'O') {
-                counterO++;
-            } else if (occ == '_' || occ == ' ') {
-                counterSpace++;
+            int counterX = 0;
+            int counterO = 0;
+            for (char occ : symbol) {
+                if (occ == 'X') {
+                    counterX++;
+                } else if (occ == 'O') {
+                    counterO++;
+                }
             }
-        }
-        if (counterX + counterO == 9 && xWins == false && oWins == false) {
-            draw = true;
-        }
-        if (Math.abs(counterX - counterO) > 1 || (xWins && oWins)) {
-            impossible = true;
-        }
+            if (counterX + counterO == 9 && xWins == false && oWins == false) {
+                draw = true;
+            }
 //Result
-        if (xWins == false && oWins == false && draw) {
-            System.out.println("Draw");
-        } else if (xWins && draw == false && impossible == false) {
-            System.out.println("X wins");
-        } else if (oWins && draw == false && impossible == false) {
-            System.out.println("O wins");
-        } else if (impossible) {
-            System.out.println("Impossible");
-        } else {
-            System.out.println("Game not finished");
-        } */
+            if (xWins == false && oWins == false && draw) {
+                System.out.println("Draw");
+                break;
+            } else if (xWins && draw == false) {
+                System.out.println("X wins");
+                break;
+            } else if (oWins && draw == false) {
+                System.out.println("O wins");
+                break;
+            }
+        } while (xWins == false || oWins == false || draw == false);
     }
 }
--- README.md
+++ README.md
@@ -1,14 +1,8 @@
 # TicTacToe
 
 # Assumption
-Analyze the game state to determine if either of the players has already won the game or it is still ongoing, if the game is a draw, or if the user has entered an impossible game state (two winners, or with one player having made too many moves).
 
-Get the 3x3 grid from the input as in the previous stages.
-Output this 3x3 grid as in the previous stages.
-Prompt the user to make a move.
-The user should input 2 numbers that represent the cell where they want to place their X. (the 9 symbols representing the field will be the first line of input, and the 2 coordinate numbers will be the second line of input)
-Analyze user input and show messages in the following situations:
-This cell is occupied! Choose another one! if the cell is not empty.
-You should enter numbers! if the user enters non-numeric symbols in the coordinates input.
-Coordinates should be from 1 to 3! if the user enters coordinates outside the game grid.
-Update the grid to include the user's move and print the updated grid to the console.
+Working TicTacToe game based on 1d array and loops.
+Prints an empty grid at the beginning of the game.
+Creates a game loop where the program asks the user to enter the cell coordinates, analyzes the move for correctness and shows a grid with the changes if everything is okay.
+Ends the game when someone wins or there is a draw.
